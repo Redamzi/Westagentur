ecaaa3d80d9988241f22de73615b2be54efefd4a
Amzi Redzep <amziredzep@Amzis-iMac.local>
2025-12-29 21:10:03 +0100

    Refactor: Move source code to src/ directory and update paths

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,9 +2,7 @@
 module.exports = {
     content: [
         "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-        "./*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}"
+        "./src/**/*.{js,ts,jsx,tsx}"
     ],
     theme: {
         extend: {
